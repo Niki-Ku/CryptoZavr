5488ebb6f32b956d94e5d2da8d122a6f999819a9
Nikita <nikita.kuzko2013@gmail.com>
2025-01-28 17:24:01 +0200

    fix: remove localization from api requests

--- next/app/(root)/layout.tsx
+++ next/app/(root)/layout.tsx
@@ -1,11 +1,12 @@
 import { ReactNode } from "react";
+import Footer from "../components/Footer/Footer";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
 	return (
 		<div className="min-h-[100svh] grid grid-rows-[auto_1fr_auto]">
 			<div>Header</div>
 			{children}
-			<div>Footer</div>
+			<Footer />
 		</div>
 	);
 };
---
+++ next/app/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+
+//TODO:
+// 
+
+
+const Footer = () => {
+	return <footer aria-label="Footer">Footer component</footer>;
+};
+
+export default Footer;
