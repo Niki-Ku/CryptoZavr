0fab2390e3aa2055bcb53da81c1f9e0063264067
Nikita <nikita.kuzko2013@gmail.com>
2025-03-04 19:07:11 +0200

    feat: implement logo component

--- next/app/components/layout/Header/Header.tsx
+++ next/app/components/layout/Header/Header.tsx
@@ -4,6 +4,7 @@ import CustomLink from "@/app/components/ui/CustomLink/CustomLink";
 import "./Header.css";
 import BurgerButton from "@/app/components/ui/BurgerButton/BurgerButton";
 import { ILink } from "@/types/types";
+import Logo from "../../ui/Logo/Logo";
 
 interface IHeader {
 	data: {
@@ -27,9 +28,7 @@ const Header: React.FC<IHeader> = ({ data, isLoggedIn }) => {
 
 	return (
 		<nav className="z-50 px-8 fixed top-0 left-0 w-full md:h-auto md:flex justify-between items-between bg-background-component">
-			{/* replace with custom logo component */}
-			<div className="text-2xl my-4">CryptoZavr</div>
-			{/* is it better to allow add className directly to component or make basic styling and the rest leave to <div> <Component ... /> </div>???  */}
+			<Logo isLink />
 			<div className="absolute z-50 w-8 right-4 top-4 md:hidden">
 				<BurgerButton isOpen={isOpen} onClick={onBurgerClick} />
 			</div>
