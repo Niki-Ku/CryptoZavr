e77d3bc9f729dda3957cf8258d1ef3b414583887
Nikita <nikita.kuzko2013@gmail.com>
2025-03-13 18:28:35 +0200

    feat: add react query provider for the app

--- next/app/layout.tsx
+++ next/app/layout.tsx
@@ -1,10 +1,13 @@
 import { ReactNode } from "react";
+import Providers from "@/providers/providers";
 import "./globals.css";
 
 const Layout = ({ children }: { children: ReactNode }) => {
 	return (
-		<html>
-			<body className="w-full h-full">{children}</body>
+		<html lang="en">
+			<body className="w-full h-full">
+				<Providers>{children}</Providers>
+			</body>
 		</html>
 	);
 };
